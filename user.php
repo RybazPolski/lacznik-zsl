25978963c7a17f784be793c123e9a148715b951f
Ryba <julorybarczyk@gmail.com>
2022-04-18 20:08:03 +0200

    fix user.php (pwd->pass)

--- user.php
+++ user.php
@@ -10,15 +10,15 @@
 <body>
     <form action="./php/login.php" method="POST">
         <label for="login">Login: </label><input type="text" name="login" required><br>
-        <label for="pwd">Hasło: </label><input type="password" name="pwd" required><br>
+        <label for="pass">Hasło: </label><input type="password" name="pass" required><br>
         <input type="submit" name="submitLogin">
     </form>
     <hr>
     <form action="./php/register.php" method="POST">
         <label for="login">Login: </label><br><input type="text" name="login" required><br>
         <label for="username">Nazwa profilu:<sub>(możesz później ją zmienić)</sub></label><br><input type="text" name="username"><br>
-        <label for="pwd">Hasło: </label><br><input type="password" name="pwd" required><br>
-        <label for="pwd2">Powtórz hasło:<sub>(wiem, irytujące ale trzeba)</sub> </label><br><input type="password" name="pwd2" required><br>
+        <label for="pass">Hasło: </label><br><input type="password" name="pass" required><br>
+        <label for="pass2">Powtórz hasło:<sub>(wiem, irytujące ale trzeba)</sub> </label><br><input type="password" name="pass2" required><br>
         <input type="submit" name="submitRegister">
     </form>
 </body>
