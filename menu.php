9b07213d1d051e4a13a8889943583894175328b9
Gierba <piootrek2005@gmail.com>
2022-04-18 20:55:52 +0200

    Szkielet strony menu.php

--- menu.php
+++ menu.php
@@ -8,6 +8,51 @@
     <title>Łącznik Online - strona naszego menu</title>
 </head>
 <body>
-    
+    <div id="main">
+        <div id="baner">
+            <h1>Bufet "Łącznik" - nasze menu</h1>
+        </div>
+        <div id="main_menu">
+            <div id="menu"><a href="user.html">Logowanie/Rejestracja</a></div>
+            <div id="menu"><a href="basket.html">Koszyk</a></div>
+            <div id="menu"><a href="profile.html">Twój profil</a></div>
+            <div id="menu"><a href="products.html">Nasze produkty</a></div>
+            <div id="menu"><a href="menu.html">Nasze menu</a></div>
+        </div>
+        <div id="left">
+            <h3>Nasze menu</h3>
+            <h4>Kotlet mielony z ziemniakami</h4>
+            <img src="" alt="Kotlet mielony z ziemniakami">
+            <p>(...)</p>
+            <h4>Domowy rosołek (jak od babci!)</h4>
+            <img src="" alt="Rosół">
+            <p>(...)</p>
+            <h4>Spaghetti bolognese</h4>
+            <img src="" alt="Spaghetti bolognese">
+            <p>(...)</p>
+            <h4>Panierowany filet z kurczaka z ziemniakami</h4>
+            <img src="" alt="Panierowany filet z kurczaka z ziemniakami">
+            <p>(...)</p>
+        </div>
+        <div id="right">
+            <h3>Aktualnie w promocji!</h3>
+            <h4>(...)</h4>
+            <img src="" alt="(...)">
+            <p>(...)</p>
+        </div>
+        <div id="footer">
+            <h4>Autorzy:</h4>
+            <ul>
+                <li>Julian Rybarczyk</li>
+                <li>Piotr Gierba</li>
+                <li>Jacek Jędra</li>
+                <li>Renata Sakhnevych</li>
+            </ul>
+            <h4>Źródła (dla strony menu):</h4>
+            <ul>
+
+            </ul>
+        </div>
+    </div>
 </body>
 </html>
