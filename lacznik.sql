049dea90d43af8b70a6b06d5a1218eeae49bd938
JedraJacek <89982768+JedraJacek@users.noreply.github.com>
2022-04-18 20:34:44 +0200

    Add files via upload

---
+++ lacznik.sql
@@ -0,0 +1,188 @@
+-- phpMyAdmin SQL Dump
+-- version 5.0.2
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Czas generowania: 18 Kwi 2022, 20:33
+-- Wersja serwera: 10.4.14-MariaDB
+-- Wersja PHP: 7.4.10
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Baza danych: `lacznik`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Struktura tabeli dla tabeli `klienci`
+--
+
+CREATE TABLE `klienci` (
+  `id` int(11) NOT NULL,
+  `login` varchar(50) NOT NULL,
+  `haslo` varchar(70) NOT NULL,
+  `nazwa` varchar(50) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Zrzut danych tabeli `klienci`
+--
+
+INSERT INTO `klienci` (`id`, `login`, `haslo`, `nazwa`) VALUES
+(1, 'test', 'Niezgadniesz', 'Stefan'),
+(2, 'dwa', '1234', 'Hermenegilda');
+
+-- --------------------------------------------------------
+
+--
+-- Struktura tabeli dla tabeli `koszyk`
+--
+
+CREATE TABLE `koszyk` (
+  `id_k` int(11) DEFAULT NULL,
+  `id_p` int(11) DEFAULT NULL,
+  `ilosc` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Struktura tabeli dla tabeli `menu`
+--
+
+CREATE TABLE `menu` (
+  `id` int(11) NOT NULL,
+  `nazwa` varchar(50) DEFAULT NULL,
+  `dzien` int(11) DEFAULT NULL,
+  `danie` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Zrzut danych tabeli `menu`
+--
+
+INSERT INTO `menu` (`id`, `nazwa`, `dzien`, `danie`) VALUES
+(1, 'kotlet mielony z ziemniakami', 1, 2),
+(2, 'rosół', 1, 1),
+(3, 'spaghetti bolognese', 3, 2),
+(4, 'panierowany filet z kurczaka z ziemniakami', 4, 2);
+
+-- --------------------------------------------------------
+
+--
+-- Struktura tabeli dla tabeli `produkty`
+--
+
+CREATE TABLE `produkty` (
+  `id` int(11) NOT NULL,
+  `nazwa` varchar(50) DEFAULT NULL,
+  `cena` float DEFAULT NULL,
+  `zapas` int(11) DEFAULT NULL,
+  `promocja` int(11) DEFAULT NULL,
+  `zdjecie` varchar(75) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Zrzut danych tabeli `produkty`
+--
+
+INSERT INTO `produkty` (`id`, `nazwa`, `cena`, `zapas`, `promocja`, `zdjecie`) VALUES
+(1, 'zeton pierwsze danie', 5, 100, 0, 'zupa'),
+(2, 'zeton drugie danie', 8, 50, 0, 'danie'),
+(3, 'zeton drugie danie z sur', 10, 50, 0, 'daniesur'),
+(4, 'snickers', 5, 10, 20, 'snickers');
+
+-- --------------------------------------------------------
+
+--
+-- Struktura tabeli dla tabeli `zamowienia`
+--
+
+CREATE TABLE `zamowienia` (
+  `id` int(11) NOT NULL,
+  `id_k` int(11) DEFAULT NULL,
+  `id_p` int(11) DEFAULT NULL,
+  `data` date DEFAULT NULL,
+  `odebrano` enum('tak','nie') DEFAULT NULL,
+  `anulowano` enum('tak','nie') DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Zrzut danych tabeli `zamowienia`
+--
+
+INSERT INTO `zamowienia` (`id`, `id_k`, `id_p`, `data`, `odebrano`, `anulowano`) VALUES
+(1, 1, 3, '2022-04-18', 'nie', 'nie'),
+(2, 1, 4, '2022-04-14', 'nie', 'tak'),
+(3, 2, 1, '2022-03-11', 'tak', 'nie');
+
+--
+-- Indeksy dla zrzutów tabel
+--
+
+--
+-- Indeksy dla tabeli `klienci`
+--
+ALTER TABLE `klienci`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indeksy dla tabeli `menu`
+--
+ALTER TABLE `menu`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indeksy dla tabeli `produkty`
+--
+ALTER TABLE `produkty`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indeksy dla tabeli `zamowienia`
+--
+ALTER TABLE `zamowienia`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT dla tabeli `klienci`
+--
+ALTER TABLE `klienci`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT dla tabeli `menu`
+--
+ALTER TABLE `menu`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- AUTO_INCREMENT dla tabeli `produkty`
+--
+ALTER TABLE `produkty`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
+--
+-- AUTO_INCREMENT dla tabeli `zamowienia`
+--
+ALTER TABLE `zamowienia`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
