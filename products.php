b22ade0645fe2c80f1af98c412f24327cad2cbf7
Gierba <piootrek2005@gmail.com>
2022-04-18 21:03:55 +0200

    Szkielet strony products.php

--- products.php
+++ products.php
@@ -8,6 +8,51 @@
     <title>Łącznik Online - lista naszych produktów</title>
 </head>
 <body>
-    
+    <div id="main">
+        <div id="baner">
+            <h1>Bufet "Łącznik" - nasze produkty</h1>
+        </div>
+        <div id="main_menu">
+            <div id="menu"><a href="user.php">Logowanie/Rejestracja</a></div>
+            <div id="menu"><a href="basket.php">Koszyk</a></div>
+            <div id="menu"><a href="profile.php">Twój profil</a></div>
+            <div id="menu"><a href="products.php">Nasze produkty</a></div>
+            <div id="menu"><a href="menu.php">Nasze menu</a></div>
+        </div>
+        <div id="left">
+            <h3>Nasze produkty</h3>
+            <h4>Snickers</h4>
+            <img src="" alt="Snickers">
+            <p>(...)</p>
+            <h4>Żeton pierwsze danie</h4>
+            <img src="" alt="Żeton pierwsze danie">
+            <p>(...)</p>
+            <h4>Żeton drugie danie</h4>
+            <img src="" alt="Żeton drugie danie">
+            <p>(...)</p>
+            <h4>Żeton drugie danie z surówką</h4>
+            <img src="" alt="Żeton drugie danie z surówką">
+            <p>(...)</p>
+        </div>
+        <div id="right">
+            <h3>Aktualnie w promocji!</h3>
+            <h4>(...)</h4>
+            <img src="" alt="(...)">
+            <p>(...)</p>
+        </div>
+        <div id="footer">
+            <h4>Autorzy:</h4>
+            <ul>
+                <li>Julian Rybarczyk</li>
+                <li>Piotr Gierba</li>
+                <li>Jacek Jędra</li>
+                <li>Renata Sakhnevych</li>
+            </ul>
+            <h4>Źródła (dla strony produkty):</h4>
+            <ul>
+
+            </ul>
+        </div>
+    </div>
 </body>
 </html>
